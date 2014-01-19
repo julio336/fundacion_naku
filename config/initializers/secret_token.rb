16ae04fd6f75936e02d34c832da5b31f428d7202
julio <julio336@hotmail.com>
2014-01-18 22:39:29 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FundacionNaku::Application.config.secret_token = '97b10ed0c727dad86ea93bcf25afdef96b98b546a8db4ea9b886e1200109f7d29ab3488be16125c9632f7b4b36a139eb2012a308472c14a0a8e234a086e20051'
